0ba318abc2e8f3f5ed8c1aa26a07dcda007d0091
Manikant Sharma <manikantsharma90@gmail.com>
2026-05-13 13:31:41 +0530

    changes the TokenType as they are updated in emum data structure

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -2,6 +2,9 @@
 #include "../include/lexer.h"
 using namespace std;
 
+// lexeme_ID : Identifier type lexeme
+// lexeme_NUM: Number type lexeme
+
 // "  let x = 5 + 6; print x;"
 vector<Token> tokenize(const string& input){
     vector<Token> tokens;
@@ -16,28 +19,29 @@ vector<Token> tokenize(const string& input){
         if(isalpha(input[i])){
             string lexeme_ID = "";
             while(isalpha(input[i])) lexeme_ID += input[i++];
-
-            if(lexeme_ID == "let" || lexeme_ID == "print") tokens.push_back({KEYWORD, lexeme_ID});
+            
+            if(lexeme_ID == "let") tokens.push_back({LET, lexeme_ID});
+            else if(lexeme_ID == "print") tokens.push_back({PRINT, lexeme_ID});
             else{
-                tokens.push_back({IDENTIFIER ,lexeme_ID});
+                tokens.push_back({IDENT, lexeme_ID});
             }
         }
 
         else if(isdigit(input[i])){
             string lexeme_NUM = "";
             while(isdigit(input[i])) lexeme_NUM += input[i++];
-            tokens.push_back({INTEGER, lexeme_NUM});
+            tokens.push_back({NUMBER, lexeme_NUM});
         }
 
         else{
             switch(input[i]){
-                case '+': tokens.push_back({OPERATOR, "+"}); break;
-                case '=': tokens.push_back({OPERATOR, "="}); break;
-                case ';': tokens.push_back({PUNCTUATION, ";"});
+                case '+': tokens.push_back({PLUS, "+"}); break;
+                case '=': tokens.push_back({ASSIGN, "="}); break;
+                case ';': tokens.push_back({SEMICOLON, ";"});
             }
             i++;
         }
     }
-
+    tokens.push_back({END, ""});
     return tokens;
 }
