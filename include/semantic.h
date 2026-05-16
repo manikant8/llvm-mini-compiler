a3b2eae35c292268eef95f8a26df10ede892a1da
Manikant Sharma <manikantsharma90@gmail.com>
2026-05-16 15:06:05 +0530

    Till Syntax Analysis Phase

---
+++ include/semantic.h
@@ -0,0 +1,2 @@
+// #include "ast.h"
+// void semantic_analyzer(ASTNode*);
---
+++ src/semantic.cpp
@@ -0,0 +1,8 @@
+// #include "../include/semantic.h"
+
+
+
+// //******************************** Semantic Analysis Function ********************************//
+// void semantic_analyzer(ASTNode* node){
+//     if()
+// }
