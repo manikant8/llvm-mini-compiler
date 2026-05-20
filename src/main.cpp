cd9f2b58b0e483115971c90a6714a52d4de97e38
Manikant Sharma <manikantsharma90@gmail.com>
2026-05-20 09:38:41 +0530

    Comments added

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,28 @@
 #include<iostream>
 #include "../include/lexer.h"
 #include "../include/parser.h"
+#include "../include/semantic.h"
 using namespace std;
 
 int main(){
-    string code = "let x = 5+6; y =8+9; print x;";
+    string code = "let x = 5+6; y = 8+9; print x; ";
 
-    // *************************** Lexical Analyser *******************************//
+    // *************************** LEXER *******************************//
     vector<Token> tokens = tokenize(code);
 
     // To display tokens
-    // for(auto &t : tokens){
-    //     cout<<t.type<<" "<<t.value<<endl;
-    // }
+    cout << "Tokens Generated <Token Type, Token>: " << endl;
+    for(auto &t : tokens){
+        cout<<t.type<<" "<<t.value<<endl;
+    }
+    cout << endl;
 
-    // *************************** Syntax Analyser *******************************//
+    // *************************** PARSER  *******************************//
     Program* program = parse(tokens);
-    cout << "Parsing is Successful\n";
+    cout << "Parsing is Successful\n" << endl;
+
+    // *************************** SEMANTIC ANALYSIS *******************************//
+    analyzeProgram(program);
+
+
 }
